a68ebc6496c5eaac0cd131e0a820e21473c33f3d
Noah Syrkis <noah@syrkis.com>
2023-07-26 17:06:32 +0200

    change font

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script context="module">
+  export async function load({ fetch }) {
+    return {
+      status: 302,
+      redirect: '/ai-services',
+    };
+  }
+</script>
+
+<script>
+  // Redirect on the client side
+  if (typeof window !== 'undefined') {
+    window.location.href = '/ai-services';
+  }
+</script>
