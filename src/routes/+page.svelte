6f1157b84a2d8ab132147c5ab16b9f5e4589c88b
Noah Syrkis <noah@syrkis.com>
2023-07-26 16:46:13 +0200

    map to .org

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  // SvelteKit hook to redirect the page on the server-side
+  import { goto } from '$app/navigation';
+  goto('https://virian.org', { replaceState: true });
+</script>
+
+<!-- Fallback client-side redirect if server-side redirect fails -->
+<meta http-equiv="refresh" content="0; URL='https://virian.org/'" />
